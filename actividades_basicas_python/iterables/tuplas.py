533cfc0e3b18efab629e58c21306c59d55cb68a3
jhonsdb <davilajhon08@gmail.com>
2024-04-29 13:21:07 +0200

    actualizacion de tupla

--- actividades_basicas_python/iterables/tuplas.py
+++ actividades_basicas_python/iterables/tuplas.py
@@ -6,6 +6,7 @@ print(tup)
 tup = ("valor_único",)
 print(tup)
 
+print("")
 
 
 animales = ("perro", "gato", "ratón")
@@ -25,29 +26,45 @@ persona = ("Persona de ejemplo", "Max", 1974, True)
 Si quieres consultar varios valores, procede de la siguiente manera: """
 animales = ("perro", "gato", "ratón", "serpiente", "caballo")
 print(animales[1:3])
+print("")
+
+""" Convertir una tupla de Python en lista """
+animales = ("perro", "gato", "ratón", "serpiente", "caballo")
+lista_animales = list(animales)  # Utilizar list() para convertir la tupla en una lista
+lista_animales[2] = "elefante"  # Modificar el elemento en el índice 2
+print(lista_animales)  # Imprimir la lista modificada
+print("")
+
 
 
 """  puedes crear una tupla y añadir valores nuevos además de la tupla original. """
 algunos_animales = ("perro", "gato", "ratón", "serpiente", "caballo")
 todos_los_animales = algunos_animales + ("hámster", "jirafa")
 print(todos_los_animales)
+print("")
 
 """ Convertir una lista en tupla  """
 lista_colores = ["azul", "rojo", "amarillo", "naranja"]
 colores = tuple(lista_colores)
 print(colores)
 print(type(colores))
+print("")
+
 
 """ eliminar una tupla """
 
 animales = ("perro", "gato", "ratón", "serpiente", "caballo")
 del animales
+print("")
+
 
 """ Con index, puedes consultar el índice de un determinado valor.  """
 animaless = ("perro", "gato", "ratón", "serpiente", "caballo")
 indice = animaless.index("serpiente")
 print("Índice de 'serpiente':", indice)
 print("Tupla de animales:", animaless)
+print("")
+
 
 """ Con count, puedes consultar cuántas veces aparece un elemento dentro de una tupla  """
 
@@ -56,5 +73,5 @@ animales = ("perro", "gato", "ratón", "serpiente", "caballo")
 cantidad_perros = animales.count("perro")
 print("Cantidad de 'perro' en la tupla:", cantidad_perros)
 print("Tupla de animales:", animales)
-
+print("")
 
